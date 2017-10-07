5b7fbfc75b4b204f9a0e77733f59e7be4b8f48cf
HOON1-PC\hoon1 <dksekfldks65@naver.com>
2017-10-08 00:21:48 +0900

    구글맵 현재 위치 마커표시 구현함

---
+++ FudiMap/app/src/main/java/fudi/fudimap/MapsActivity.java
@@ -0,0 +1,143 @@
+package fudi.fudimap;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    static double longi;
+    static double lati;
+    static int i = 0;
+    int cnt = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        // 화면을 portrait 세로화면으로 고정
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        GPSListener gpsListener = new GPSListener();
+        long minTime = 60000;
+        float minDistance = 0;
+
+
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            manager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    minTime,
+                    minDistance,
+                    gpsListener);
+        }
+
+        catch (Exception E) {
+            manager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    minTime,
+                    minDistance,
+                    gpsListener);
+        }
+
+        Toast.makeText(getApplicationContext(), "위치 확인 시작함. 로그를 확인하세요.", Toast.LENGTH_SHORT).show();
+    }
+
+    private class GPSListener implements LocationListener {
+        Double latitude;
+        Double longitude;
+        Location location;
+
+        public void onLocationChanged(Location location) {
+            //capture location data sent by current provider
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+
+            lati = latitude;
+            longi = longitude;
+
+            showCurrentLocation(latitude,longitude);
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+            lati = location.getLatitude();
+            longi = location.getLongitude();
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    }
+
+    private void showCurrentLocation(final Double latitude, final Double longitude) {
+        /*
+        // 현재 위치를 이용해 LatLon 객체 생성
+        mMap.clear();
+        LatLng curPoint = new LatLng(latitude, longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(curPoint));
+
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        MarkerOptions optFirst = new MarkerOptions();
+        optFirst.position(curPoint);// 위도 • 경도
+        optFirst.title("Current Position");// 제목 미리보기
+        optFirst.snippet("Snippet");
+        optFirst.icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+        mMap.addMarker(optFirst).showInfoWindow();
+        */
+
+        LatLng curPoint = new LatLng(latitude, longitude);
+
+        if(cnt ==0) {
+            //마커객체 생성
+            MarkerOptions optSecond = new MarkerOptions();
+            optSecond.position(new LatLng(latitude, longitude));// 위도 • 경도
+            optSecond.title("현재위치"); // 제목 미리보기
+            mMap.addMarker(optSecond).showInfoWindow();
+            cnt++;
+        }
+
+        //currentPosition 위치로 카메라 중심을 옮기고 화면 줌을 조정한다. 줌범위는 2~21, 숫자클수록 확대
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 17));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17), 2000, null);
+
+        // 지도 유형 설정. 지형도인 경우에는 GoogleMap.MAP_TYPE_TERRAIN, 위성 지도인 경우에는 GoogleMap.MAP_TYPE_SATELLITE
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+}
