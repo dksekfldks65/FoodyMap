67edf45675021c89fe09c670abbe9a7090e218bc
HOON1-PC\hoon1 <dksekfldks65@naver.com>
2017-10-10 14:12:26 +0900

    맛집 리스트를 기록할 ListView 구현함

---
+++ FudiMap/app/src/main/java/fudi/fudimap/ListItem.java
@@ -0,0 +1,34 @@
+package fudi.fudimap;
+
+
+import android.graphics.drawable.Drawable;
+
+public class ListItem {
+    private Drawable iconDrawable ;
+    private String titleStr ;
+    private String descStr ;
+    private int id;
+
+    public void setIcon(Drawable icon) {
+        iconDrawable = icon ;
+    }
+    public void setTitle(String title) {
+        titleStr = title ;
+    }
+    public void setDesc(String desc) {
+        descStr = desc ;
+    }
+    public void setId(int id) {this.id=id;}
+
+    public Drawable getIcon() {
+        return this.iconDrawable ;
+    }
+    public String getTitle() {
+        return this.titleStr ;
+    }
+    public String getDesc() {
+        return this.descStr ;
+    }
+    public int getId() {return this.id;}
+
+}
